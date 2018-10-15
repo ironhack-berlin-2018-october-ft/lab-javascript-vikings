b34207001e2e2891c9737761fbe6889ca3cd5fd9
“Eullin <eullinch@gmail.com>
2018-10-15 07:22:05 +0200

    done

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -60,8 +60,35 @@ class War {
         this.saxonArmy.push(saxonobject);
     }
     vikingAttack(){
-        
+        var randomViking = Math.floor(Math.random()*this.vikingArmy.length)
+        var randomSaxon = Math.floor(Math.random()*this.saxonArmy.length)
+        var tag = this.saxonArmy[randomSaxon].receiveDamage(this.vikingArmy[randomViking].strength)
+        if (this.saxonArmy[randomSaxon].health <= 0) {
+            this.saxonArmy.splice(randomSaxon, 1);
+          }
+          console.log(tag)
+          return tag
     }
-    saxonAttack(){}
-    showStatus(){}
+    saxonAttack(){
+        let randomViking = Math.floor(Math.random()*this.vikingArmy.length)
+        let randomSaxon = Math.floor(Math.random()*this.saxonArmy.length)
+        var rem = this.vikingArmy[randomViking].receiveDamage(this.saxonArmy[randomSaxon].strength)
+        if (this.vikingArmy[randomViking].health <= 0) {
+            this.vikingArmy.splice(randomViking, 1);
+          }
+        console.log(rem)
+        return rem
+    }
+    showStatus(){
+        if(this.saxonArmy.length===0){
+            return "Vikings have won the war of the century!";
+        } else if (this.vikingArmy.length===0) {
+            return "Saxons have fought for their lives and survive another day...";
+        } else {
+            return "Vikings and Saxons are still in the thick of battle.";
+        }
+    }
 }
+
+//Armies Attack 
+
